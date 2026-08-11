f9954d14e76856aea6acfd021f715ab3da510940
guigocks-ux <guigocks@gmail.com>
2026-08-11 20:57:57 -0300

    Inizializacao do springboot + organizacao de pastas + gitignore

---
+++ api/src/main/java/AJTBackend/AJTBackendApplication.java
@@ -0,0 +1,11 @@
+package AJTBackend;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AJTBackendApplication {
+
+
+}
