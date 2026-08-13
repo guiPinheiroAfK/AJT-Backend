9510dd82ee9fc2a891f4901130d6f21f5f480811
guigocks-ux <guigocks@gmail.com>
2026-08-13 17:59:13 -0300

    Dockerfile e docker-compose.yml criados

--- api/src/main/java/AJTBackend/AJTBackendApplication.java
+++ api/src/main/java/AJTBackend/AJTBackendApplication.java
@@ -1,11 +1,12 @@
 package AJTBackend;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AJTBackendApplication {
 
-
+    public static void main(String[] args) {
+        SpringApplication.run(AJTBackendApplication.class, args);
+    }
 }
